a7cf0c17a6548205109c6ce218c8e737ce51e22c
Andrew Kushnir <akushnir2009@gmail.com>
2020-08-29 13:53:12 -0600

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { OrganizerComponent } from './organizer/organizer.component';
+import { SelectorComponent } from './selector/selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavigationComponent,
+    CalendarComponent,
+    OrganizerComponent,
+    SelectorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
