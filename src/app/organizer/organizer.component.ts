0632474221ff68d49342351830ca036862646760
Andrew Kushnir <akushnir2009@gmail.com>
2020-09-01 21:10:25 -0600

    Organizer added

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { faTrash, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import {DateService} from '../shared/date.service';
 
 @Component({
   selector: 'app-organizer',
   templateUrl: './organizer.component.html',
-  styleUrls: ['./organizer.component.css']
+  styleUrls: ['./organizer.component.scss']
 })
 export class OrganizerComponent implements OnInit {
+  faTrash = faTrash;
+  faPlusCircle = faPlusCircle;
 
-  constructor() { }
+  constructor(public dateService: DateService) {
+    dateService.date.subscribe(this.getNotes.bind(this));
+  }
 
   ngOnInit(): void {
   }
 
+  getNotes(): void {
+    const date = this.dateService.date;
+  }
 }
