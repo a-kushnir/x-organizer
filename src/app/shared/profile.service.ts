c35599a2077471cc21642de3d63eac75f2f97cbe
Andrew Kushnir <akushnir2009@gmail.com>
2020-09-28 21:48:31 -0600

    Production error

--- src/app/shared/profile.service.ts
+++ src/app/shared/profile.service.ts
@@ -11,8 +11,8 @@ export class ProfileService {
 
   constructor(private userService: UserService) {
     this.theme = new BehaviorSubject<string>(ProfileService.loadTheme());
-    alert(`src/styles/${this.theme.value}-theme.scss`); // debug problem in heroku
-    require(`src/styles/${this.theme.value}-theme.scss`);
+    // alert(`src/styles/${this.theme.value}-theme.scss`); // debug problem in heroku
+    // require(`src/styles/${this.theme.value}-theme.scss`);
 
     this.theme.subscribe(theme => {
       ProfileService.saveTheme(theme);
