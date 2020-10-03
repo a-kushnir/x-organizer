8c6a7a2d91ae0d0c119dcabf7e18cb925ed7d8bb
Andrew Kushnir <akushnir2009@gmail.com>
2020-10-03 14:04:16 -0600

    Real Time DB update

--- src/app/shared/profile.service.ts
+++ src/app/shared/profile.service.ts
@@ -21,7 +21,7 @@ export class ProfileService {
     userService.user.subscribe(user => {
       if (user && user.theme !== this.theme.value) {
         this.theme.next(user.theme);
-        setTimeout(location.reload.bind(location), 100);
+        setTimeout(location.reload.bind(location), 1000);
       }
     });
   }
