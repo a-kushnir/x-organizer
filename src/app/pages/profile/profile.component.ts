c9ec897d771b5ce1a122a8b2b1105d0ccd494340
Andrew Kushnir <akushnir2009@gmail.com>
2020-10-04 20:06:37 -0600

    Theme update

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {LocalStorage} from '../../shared/local-storage';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
+  card: string;
+  private state: string;
 
   constructor() {
+    this.card = LocalStorage.getString('profile-card') ?? 'account';
+    this.state = this.card;
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    LocalStorage.setString('profile-card', null);
+  }
+
+  toggle(card: string): void {
+    if (this.state === card) {
+      this.state = '';
+    } else {
+      this.state = card;
+    }
+    LocalStorage.setString('profile-card', this.state);
   }
 }
