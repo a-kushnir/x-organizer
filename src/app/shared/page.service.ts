73150b3007acbf9c8189cdb8c8f704accc52c666
Andrew Kushnir <akushnir2009@gmail.com>
2020-10-05 20:28:29 -0600

    Title Added

--- src/app/shared/page.service.ts
+++ src/app/shared/page.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
 import {LocalStorage} from './local-storage';
+import {Title} from '@angular/platform-browser';
 
 export enum Pages {
   Home = 'Home',
@@ -13,14 +14,16 @@ export enum Pages {
   providedIn: 'root'
 })
 export class PageService {
+  static titles = {SignIn: 'Sign In', SignUp: 'Sign Up'};
   page: BehaviorSubject<Pages>;
 
-  constructor() {
+  constructor(private titleService: Title) {
     this.page = new BehaviorSubject<Pages>(
       Pages[LocalStorage.getString('page') ?? Pages.SignIn]
     );
     this.page.subscribe(page => {
       LocalStorage.setString('page', page);
+      titleService.setTitle(`${PageService.titles[page] ?? page} | xOrganizer`);
     });
   }
 }
